53683c8f9e3d57c4e66d7ebd98757e9473d6ebe9
Qiuao <804980745@qq.com>
2021-09-05 20:24:10 +0800

    修改传给前端的数据，加上qid,oid

--- question-system/src/main/java/com/heu/system/domain/Option.java
+++ question-system/src/main/java/com/heu/system/domain/Option.java
@@ -4,8 +4,17 @@ package com.heu.system.domain;
  * 答案类
  */
 public class Option {
+    private int oid;
     private String optionDec;//答案描述
     private int answer;//问题答案 0：错误  1：正确
+
+    public int getOid() {
+        return oid;
+    }
+
+    public void setOid(int oid) {
+        this.oid = oid;
+    }
 
     public String getOptionDec() {
         return optionDec;
